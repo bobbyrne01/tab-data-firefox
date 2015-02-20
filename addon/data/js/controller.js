143442a80d1046ffd57bc495fd58976f2e154697
Robert Byrne <bobbyrne01@gmail.com>
2015-02-20 14:16:34 +0000

    Settings added to Panel

--- addon/data/js/controller.js
+++ addon/data/js/controller.js
@@ -1,7 +1,44 @@
+/*
+ * Event listeners
+ */
+document.getElementById('memoryTrackingPref').addEventListener("change", function (event) {
+	
+	if (document.getElementById('memoryTrackingPref').checked) {
+		
+		self.port.emit("memoryTrackingSetting", true);
+		document.getElementById('memoryIntervalPref').disabled = false;
+		
+	} else {
+		
+		self.port.emit("memoryTrackingSetting", false);
+		document.getElementById('memoryIntervalPref').disabled = true;
+	}
+}, false);
+
+document.getElementById('memoryIntervalPref').onkeyup = function (event) {
+	
+	if (document.getElementById('memoryIntervalPref').value >= 1){
+		
+		self.port.emit("memoryIntervalSetting", document.getElementById('memoryIntervalPref').value);
+		document.getElementById('memoryIntervalPref').className = 'green';
+		
+	}else{
+		
+		document.getElementById('memoryIntervalPref').className = 'red';
+	}
+};
+
+
+
+/*
+ * Listen for add-on messages
+ */
 self.port.on("stats", function (stats) {
 	var parsedStats = JSON.parse(stats);
 
 	document.getElementById("globalCount").value = parsedStats.globalCount;
 	document.getElementById("sessionCount").value = parsedStats.sessionCount;
 	document.getElementById("currentCount").value = parsedStats.currentCount;
+	document.getElementById("memoryTrackingPref").checked = parsedStats.memoryTracking;
+	document.getElementById("memoryIntervalPref").value = parsedStats.memoryInterval;
 });
--- addon/lib/Panel.js
+++ addon/lib/Panel.js
@@ -2,13 +2,14 @@ var Panel = require("sdk/panel"),
 	Tab = require("./Tab"),
 	Data = require("./Data"),
 	Button = require("./ToggleButton"),
+	Preference = require("./Preference"),
 	panel;
 
 exports.init = function () {
 
 	panel = Panel.Panel({
-		width: 400,
-		height: 125,
+		width: 500,
+		height: 300,
 		contentURL: Data.get("html/view.html"),
 		contentScriptFile: Data.get("js/controller.js"),
 		onShow: function () {
@@ -16,7 +17,9 @@ exports.init = function () {
 			var stats = JSON.stringify({
 				globalCount: Tab.getGlobalCount(),
 				sessionCount: Tab.getSessionCount(),
-				currentCount: Tab.getCurrentCount()
+				currentCount: Tab.getCurrentCount(),
+				memoryTracking: Preference.get("memoryTracking"),
+				memoryInterval: Preference.get("memoryInterval")
 			});
 
 			panel.port.emit("stats", stats);
@@ -26,6 +29,14 @@ exports.init = function () {
 				checked: false
 			});
 		}
+	});
+	
+	panel.port.on("memoryTrackingSetting", function (value) {
+		Preference.set('memoryTracking', value);
+	});
+	
+	panel.port.on("memoryIntervalSetting", function (value) {
+		Preference.set('memoryInterval', value);
 	});
 };
 
